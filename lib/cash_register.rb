15687a97156e0ebd3be59767fc29d2af3381402d
Tina Shi <init.defaultBranch>
2023-05-11 17:33:30 +1000

    commit tree fa31152a981fb1ae8c145dc5832eedd1fd357078

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+    attr_accessor :total
+    attr_reader :discount, :items
+    
+    def initialize(discount=0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price, quantity=1)
+        self.total = self.total + (price * quantity)
+        self.items
+    end
+
+    def items=(item)
+        @items << item
+    end
+
+    def apply_discount
+        if self.discount > 0
+            self.total = self.total - (self.total * self.discount/100)
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+end
 
